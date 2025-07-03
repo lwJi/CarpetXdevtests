22e3f640b086983f0f3bbc014f3896b6ca47b135
Liwei Ji <jiliwei.phys@gmail.com>
2025-07-03 16:38:08 -0400

    TestRestrict: use gaussian profile

--- TestRestrict/src/restrict.cxx
+++ TestRestrict/src/restrict.cxx
@@ -5,14 +5,39 @@
 
 #define GFINDEXTYPE 1, 1, 1
 
+// u(t,r) = (f(t-r) - f(t+r)) / r
+// f(v) = A exp(-1/2 (r/W)^2)
+template <typename T>
+constexpr void gaussian(const T A, const T W, const T t, const T x, const T y,
+                        const T z, T &u, T &rho) {
+  using std::exp, std::pow, std::sqrt;
+
+  const T r = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+  const auto f = [&](const T v) {
+    return A * exp(-pow(v, 2) / (2 * pow(W, 2)));
+  };
+
+  if (r < sqrt(std::numeric_limits<T>::epsilon())) {
+    // L'Hôpital
+    u = 2 / pow(W, 2) * f(t) * t;
+    rho = -2 / pow(W, 4) * f(t) * (pow(t, 2) - pow(W, 2));
+  } else {
+    u = (f(t - r) - f(t + r)) / r;
+    rho = -(f(t - r) * (t - r) - f(t + r) * (t + r)) / (pow(W, 2) * r);
+  }
+}
+
 extern "C" void TestRestrict_Init(CCTK_ARGUMENTS) {
   DECLARE_CCTK_PARAMETERS;
   DECLARE_CCTK_ARGUMENTSX_TestRestrict_Init;
 
   CCTK_INFO("Initializing grid function");
-  grid.loop_all<GFINDEXTYPE>(grid.nghostzones, [=](const Loop::PointDesc &pt) {
-    CCTK_REAL canary = 100 + 10 * cctk_iteration + 1 * cctk_level;
-    iteration(pt.I) = canary;
+  grid.loop_all<GFINDEXTYPE>(grid.nghostzones, [=](const Loop::PointDesc &p) {
+    const CCTK_REAL amplitude = 1.0;
+    const CCTK_REAL gaussian_width = 0.1;
+    CCTK_REAL u, rho;
+    gaussian(amplitude, gaussian_width, cctk_time, p.x, p.y, p.z, u, rho);
+    iteration(p.I) = u;
   });
 }
 
@@ -23,8 +48,11 @@ extern "C" void TestRestrict_Update(CCTK_ARGUMENTS) {
   CCTK_VINFO("Updating grid function at iteration %d level %d time %g",
              cctk_iteration, cctk_level, cctk_time);
   grid.loop_int<GFINDEXTYPE>(
-      grid.nghostzones, [=] CCTK_HOST(const Loop::PointDesc &pt) {
-        CCTK_REAL canary = 100 + 10 * cctk_iteration + 1 * cctk_level;
-        iteration(pt.I) = canary;
+      grid.nghostzones, [=] CCTK_HOST(const Loop::PointDesc &p) {
+        const CCTK_REAL amplitude = 1.0;
+        const CCTK_REAL gaussian_width = 0.1;
+        CCTK_REAL u, rho;
+        gaussian(amplitude, gaussian_width, cctk_time, p.x, p.y, p.z, u, rho);
+        iteration(p.I) = u;
       });
 }
